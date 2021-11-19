49ae23e34053a809e20e32aed0b8e6b1c06d439b
vrojas <experthomeshop@gmail.com>
2021-11-18 22:56:01 -0400

    codigos nuevos

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
-a = "hello world"
-b  = ", SIGNIFICA HOLA MUNDO"
+#from flask import Flask
 
-print (a+b)
+#app = Flask (__name__)
+
+#    @app.route('/')
+#    def index():
+#        return 'HELLO WORLD'
+
+#if __name__ == '__main__':
+#    app.run (port = 3000, debug = True)
+
+a= 4+4
+b= 5*5+2+5
+print(b+a)
 
